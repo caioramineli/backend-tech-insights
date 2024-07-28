c978e82982faa550108737b0eb29d752ca986122
caioramineli <caiofrancoramineli3@gmail.com>
2024-07-28 18:24:48 -0300

    ajustes

--- index.js
+++ index.js
@@ -151,11 +151,11 @@ app.post("/login", async (req, res) => {
     }
 });
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+// const dbUser = process.env.DB_USER
+// const dbPassword = process.env.DB_PASS
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.yjh5qvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
-    app.listen(port)
-    console.log("Conectou!!");
-}).catch((err) => console.log(err))
+// mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.yjh5qvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+//     app.listen(port)
+//     console.log("Conectou!!");
+// }).catch((err) => console.log(err))
 
