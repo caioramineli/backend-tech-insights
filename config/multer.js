68510ad3ad13dced684b1dfef82d7a671e2921d0
caioramineli <caiofrancoramineli3@gmail.com>
2024-08-02 15:13:56 -0300

    ajustes na api para receber arquivos

--- config/multer.js
+++ config/multer.js
@@ -1,15 +1,55 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, "imgs/");
     },
     filename: function (req, file, callback) {
-        callback(null, Date.now() + path.extname(file.originalname));
-    },
+        fs.readdir("imgs/", (err, files) => {
+            if (err) {
+                return callback(err);
+            }
+            const fileCount = files.length + 1;
+            const extension = path.extname(file.originalname);
+            const uniqueName = `${fileCount}${extension}`;
+            callback(null, uniqueName);
+        });
+    }
 });
 
 const upload = multer({ storage });
 
 module.exports = upload;
+
+// Outros codigos
+// function formatDate(date) {
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const month = String(date.getMonth() + 1).padStart(2, '0'); // Os meses são baseados em zero, então adicionamos 1
+//     const year = date.getFullYear();
+
+//     const hours = String(date.getHours()).padStart(2, '0');
+//     const minutes = String(date.getMinutes()).padStart(2, '0');
+//     const seconds = String(date.getSeconds()).padStart(2, '0');
+
+//     return `${day}-${month}-${year}-${hours}-${minutes}-${seconds}`;
+// }
+
+// const multer = require("multer");
+// const path = require("path");
+// const { v4: uuidv4 } = require("uuid");
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, "imgs/");
+//     },
+//     filename: function (req, file, callback) {
+//         const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
+//         callback(null, uniqueName);
+//     },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
--- controller/productController.js
+++ controller/productController.js
@@ -1,8 +1,18 @@
 const fs = require("fs");
 const Product = require('../models/Product')
+const upload = require('../config/multer');
 
 function productController(app) {
-    app.post('/product/create', async (req, res) => {
+    app.post("/product/create", upload.array("images", 5), async (req, res) => {
+        if (!req.files || req.files.length !== 5) {
+            if (req.files) {
+                req.files.forEach(file => fs.unlink(file.path, err => {
+                    if (err) console.error(`Erro ao deletar o arquivo: ${file.path}`);
+                }));
+            }
+            return res.status(400).json({ msg: "Exatamente 5 imagens são necessárias" });
+        }
+
         const {
             nome,
             precoPrazo,
@@ -12,15 +22,9 @@ function productController(app) {
             marca,
             categoria,
             estoque
-        } = req.body
+        } = req.body;
 
-        const {
-            img1,
-            img2,
-            img3,
-            img4,
-            img5,
-        } = req.file
+        const imgPaths = req.files.map(file => file.path);
 
         const product = new Product({
             nome,
@@ -30,34 +34,37 @@ function productController(app) {
             especificacoes,
             marca,
             categoria,
-            img1: img1.path,
-            img2: img2.path,
-            img3: img3.path,
-            img4: img4.path,
-            img5: img5.path,
+            img1: imgPaths[0],
+            img2: imgPaths[1],
+            img3: imgPaths[2],
+            img4: imgPaths[3],
+            img5: imgPaths[4],
             estoque
-        })
+        });
 
         try {
             await product.save();
-            res.status(201).json({ msg: 'Produto cadastrado com sucesso!' })
+            res.status(201).json({ msg: "Produto cadastrado com sucesso!" });
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ msg: "Erro no servidor!" })
+            console.error(error);
+            res.status(500).json({ msg: "Erro no servidor!" });
         }
-    })
+    });
 
     app.get("/product/:id", async (req, res) => {
-        const id = req.params.id
+        const id = req.params.id;
 
-        const product = await Product.findById(id)
-
-        if (!product) {
-            return res.status(404).json({ msg: "Produto não encontrado!" })
+        try {
+            const product = await Product.findById(id);
+            if (!product) {
+                return res.status(404).json({ msg: "Produto não encontrado!" });
+            }
+            res.status(200).json({ product });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: "Erro no servidor!" });
         }
-
-        res.status(200).json({ product })
-    })
+    });
 }
 
 module.exports = { productController };
